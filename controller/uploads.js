008a61f989e15993b1f3d7156c37b97768943561
THE FABI7 <fabianguancha13@gmail.com>
2023-09-17 15:49:20 -0500

    helper- subir archivo

--- controller/uploads.js
+++ controller/uploads.js
@@ -1,32 +1,16 @@
 const express = require("express");
-const path = require("path");
-const { v4: uuidv4 } = require("uuid");
+const { subirArchivo } = require("../helpers/subir-archivos");
 
-const cargarArchivo = (req, res) => {
+const cargarArchivo = async (req, res) => {
   if (!req.files || Object.keys(req.files).length == 0 || !req.files.archivo) {
     res.status(400).send("No files were upload");
     return;
   }
-  const { archivo } = req.files;
-  const nombreCortado = archivo.name.split(".");
-  const extension = nombreCortado[nombreCortado.length - 1];
 
-  //extensiones validas
-  const extensionesValidas = ["png", "jpg", "jpeg", "gif"];
-  if (!extensionesValidas.includes(extension)) {
-    return res.status(400).json({
-      msg: "el tipo de extension utilizada no es permitida",
-    });
-  }
-  
-  const nombreTemp = uuidv4() + "." + extension;
-  const uploadFiles = path.join(__dirname, "../uploads/", nombreTemp);
+  const nombre = await subirArchivo(req.files);
 
-  archivo.mv(uploadFiles, (err) => {
-    if (err) {
-      return res.status(500).json({ err });
-    }
-    res.json({ msg: "file uploads  to: " + uploadFiles });
+  res.json({
+    nombre,
   });
 };
 
